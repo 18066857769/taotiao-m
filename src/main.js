c7534a9a6df80bb876575a82d2e1c32b32a92452
return <1006078856@qq.com>
2020-10-27 11:32:35 +0800

    项目初始化-引入vant组件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import '../src/styes/index.less'
+import '../src/styes/icon.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
+// 注册使用vant组件库
+Vue.use(Vant)
 
 new Vue({
   router,
